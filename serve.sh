af411d01925265631250daa7679a4fc9033c9d3b
Alfredo Canziani <alfredo.canziani@gmail.com>
2025-06-09 16:27:30 -0400

    serve.sh is now gem version independent

--- serve.sh
+++ serve.sh
@@ -1,2 +1,9 @@
+# Locally builds and serves the website #######################################
+# /opt/homebrew/opt/ruby -> ../Cellar/ruby/<latest-version>
+# gem env gemdir -> /opt/homebrew/lib/ruby/gems/<latest-version>
+$(/opt/homebrew/opt/ruby/bin/gem env gemdir)/bin/jekyll serve --trace $@
+###############################################################################
+
+# Old commands
 # /usr/local/lib/ruby/gems/3.1.0/bin/jekyll serve --trace $@
-/opt/homebrew/lib/ruby/gems/3.3.0/bin/jekyll serve --trace $@
+# /opt/homebrew/lib/ruby/gems/3.3.0/bin/jekyll serve --trace $@
